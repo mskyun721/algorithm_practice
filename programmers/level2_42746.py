58b630131bbed838c91647f7d2106e045b72173c
msk <mskyun721@gmail.com>
2022-08-01 21:14:12 +0900

    가장 큰수 수정

--- programmers/level2_42746.py
+++ programmers/level2_42746.py
@@ -3,16 +3,14 @@
 # 1 <= numbers 길이 <= 100,000
 # numbers 원소 : 0 ~ 1,000
 
-import itertools
 
 def solution(numbers):
-    answer = ''
     numbers = [str(i) for i in numbers]
     numbers.sort(reverse=True)
     print(numbers)
 
     for i in range(0, len(numbers) - 1):
-        if numbers[i] + numbers[i+1] < numbers[i+1] + numbers[i]:
+        if int(numbers[i] + numbers[i+1]) < int(numbers[i+1] + numbers[i]:)
             numbers[i+1], numbers[i] = numbers[i], numbers[i+1]
 
     print(numbers)
